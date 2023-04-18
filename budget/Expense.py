5c6c7f858b2a069044e18b6a7454bc87183180e0
killearnrui <kangrui2002@yahoo.com>
2023-04-18 15:43:25 -0400

    Update Expense.py

--- budget/Expense.py
+++ budget/Expense.py
@@ -26,22 +26,7 @@ class Expenses():
                 self.sum += amount
   
 
-    
- def categorize_set_comprehension(self):
-        necessary_expenses = {x for x in self.list 
-                            if x.category == 'Phone' or x.category == 'Auto and Gas' or 
-                                x.category == 'Classes'  or x.category == 'Utilities' or 
-                                x.category == 'Mortgage'}
-
-        food_expenses = {x for x in self.list 
-                            if x.category == 'Groceries' or x.category == 'Eating Out'}
-
-        unnecessary_expenses = set(self.list) - necessary_expenses - food_expenses
-
-        return [necessary_expenses, food_expenses, unnecessary_expenses]
-
-
-def categorize_for_loop(self):
+    def categorize_for_loop(self):
             necessary_expenses2 = set()
             food_expenses2 = set()
             unnecessary_expenses2 = set()
@@ -56,4 +41,19 @@ def categorize_for_loop(self):
                     unnecessary_expenses2.add(i)
 
             return [necessary_expenses2, food_expenses2, unnecessary_expenses2]
+
+
+ def categorize_set_comprehension(self):
+        necessary_expenses = {x for x in self.list 
+                            if x.category == 'Phone' or x.category == 'Auto and Gas' or 
+                                x.category == 'Classes'  or x.category == 'Utilities' or 
+                                x.category == 'Mortgage'}
+
+        food_expenses = {x for x in self.list 
+                            if x.category == 'Groceries' or x.category == 'Eating Out'}
+
+        unnecessary_expenses = set(self.list) - necessary_expenses - food_expenses
+
+        return [necessary_expenses, food_expenses, unnecessary_expenses]
+
 
